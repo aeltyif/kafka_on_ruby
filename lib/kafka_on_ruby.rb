e6983d3f4e68603057b82a60f35fc669ed48ad8a
Ahmad Albu-Eltyif <albueltyif@icloud.com>
2025-03-15 09:46:43 +0000

    Clean up

--- lib/kafka_on_ruby.rb
+++ lib/kafka_on_ruby.rb
@@ -9,7 +9,7 @@ module KafkaOnRuby
     argument :project_name, type: :string
 
     def self.source_root
-      File.expand_path("../../templates", __dir__) # Ensures templates are used
+      File.expand_path("../../templates", __dir__)
     end
 
     def create_project_structure
@@ -50,7 +50,6 @@ module KafkaOnRuby
         SCHEMA_URL=http://localhost:8089
         SCHEMA_USERNAME=consumer
         SCHEMA_PASSWORD=secret
-        ENABLE_QUERY_LOG=true
       ENV
     end
   end
--- lib/kafka_on_ruby/cli.rb
+++ lib/kafka_on_ruby/cli.rb
@@ -4,6 +4,7 @@ require "thor"
 require "kafka_on_ruby"
 
 module KafkaOnRuby
+  # This class generates the project
   class CLI < Thor
     desc "new PROJECT_NAME", "Create a new project"
     def new(project_name)
